4b86def8284ee7cecce1314f255c3ef9dcbfa8bf
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-07-01 15:57:25 +0100

    cat Page, shadcn UI Table and subtotal Card

--- lib/utils.ts
+++ lib/utils.ts
@@ -52,8 +52,18 @@ export function round2(value: number | string) {
   }
 }
 
-/*const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
   currency: "USD",
   style: "currency",
   minimumFractionDigits: 2,
-});*/
+});
+
+export function formatCurrency(amount: number | string | null) {
+  if (typeof amount === "number") {
+    return CURRENCY_FORMATTER.format(amount);
+  } else if (typeof amount === "string") {
+    return CURRENCY_FORMATTER.format(Number(amount));
+  } else {
+    return "NaN";
+  }
+}
